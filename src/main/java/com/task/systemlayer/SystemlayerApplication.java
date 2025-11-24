46fd8505bb4f55720bfc2592204723032584b7a9
Basel Bashir <eng.baselbashir@gmail.com>
2025-11-25 00:37:58 +0300

    First commit

---
+++ src/main/java/com/task/systemlayer/SystemlayerApplication.java
@@ -0,0 +1,13 @@
+package com.task.systemlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemlayerApplication.class, args);
+	}
+
+}
